098b5b4fa55a5bcfbf556061839c0a182571f355
zjafarey <76854058+zjafarey@users.noreply.github.com>
2021-03-02 15:34:01 +0400

    Update Zaid Symbol.fs

--- Zaid Symbol.fs
+++ Zaid Symbol.fs
@@ -68,7 +68,7 @@ type CompProps = {
         Id : SymbolId
         Pos : XYPos
         Type : ComponentType
-        NumOfUpInputs : int
+        NumOfUpwardInputs : int
         H : float
         W : float
         HeaderMargin : float
@@ -80,12 +80,19 @@ let generatePortList compProps numOfPorts portType connectionDirection =
     
     let busWidth portNum = 
         match compProps.Type, portType, portNum with
-        | Decode4, PortType.Input, 0 -> 2
-        | _ -> 1
+        | Decode4, PortType.Input, 0 -> Some 2
+        | NbitsAdder _, PortType.Input, 0 -> Some 1
+        | NbitsAdder bw, PortType.Input, _ -> Some bw
+        | NbitsAdder bw, PortType.Output, 0 -> Some bw
+        | NbitsAdder _, PortType.Output, _ -> Some 1
+        | Input bw, _, _ | Output bw, _, _ -> Some bw
+        | BusSelection _, PortType.Input, _ -> None
+        | BusSelection (bw,_), PortType.Output, _ -> Some bw
+        | _ -> Some 1
 
     match portType,connectionDirection with
     
-    | PortType.Input,Up -> 
+    | PortType.Input, Up -> 
         let portDists = compProps.W/float(numOfPorts+1)
         let relativePortPos i = 
             match compProps.Type with
@@ -100,12 +107,27 @@ let generatePortList compProps numOfPorts portType connectionDirection =
                 PortType = portType
                 PortPos = {X = compProps.Pos.X + (relativePortPos i).X ; Y = compProps.Pos.Y + (relativePortPos i).Y}
                 RelativePortPos = relativePortPos i
-                BusWidth = Some (busWidth i)
+                BusWidth = busWidth i
                 ConnectionDirection = connectionDirection
                 HostId = compProps.Id
             })
 
-    | PortType.Input,_ -> 
+    | PortType.Input, Down -> 
+        let portDists = compProps.W/float(numOfPorts+1)
+        [1..numOfPorts] 
+        |> List.map (fun i -> 
+            {
+                Id = Helpers.uuid()
+                PortNumber = Some (i-1)
+                PortType = portType
+                PortPos = {X = compProps.Pos.X + float(i)*portDists ; Y = compProps.Pos.Y}
+                RelativePortPos = {X = float(i)*portDists ; Y = 0.}
+                BusWidth = busWidth i
+                ConnectionDirection = connectionDirection
+                HostId = compProps.Id
+            })
+
+    | PortType.Input, _ -> 
         let portDists = (compProps.H - compProps.HeaderMargin - compProps.FooterMargin)/float(numOfPorts+1)
         [1..numOfPorts] 
         |> List.map (fun i -> 
@@ -115,12 +137,12 @@ let generatePortList compProps numOfPorts portType connectionDirection =
                 PortType = portType
                 PortPos = {X = compProps.Pos.X ; Y = compProps.Pos.Y + compProps.HeaderMargin + (float(i)*portDists)}
                 RelativePortPos = {X = 0. ; Y = compProps.HeaderMargin + (float(i)*portDists)}
-                BusWidth = Some (busWidth i)
+                BusWidth = busWidth i
                 ConnectionDirection = connectionDirection
                 HostId = compProps.Id
             })
 
-    | PortType.Output,_ -> 
+    | PortType.Output, _ -> 
         let portDists = (compProps.H - compProps.HeaderMargin - compProps.FooterMargin)/float(numOfPorts+1)
         [1..numOfPorts] 
         |> List.map (fun i -> 
@@ -130,7 +152,7 @@ let generatePortList compProps numOfPorts portType connectionDirection =
                 PortType = portType
                 PortPos = {X = compProps.Pos.X + compProps.W ; Y = compProps.Pos.Y + compProps.HeaderMargin + (float(i)*portDists)}
                 RelativePortPos = {X = compProps.W ; Y = compProps.HeaderMargin + (float(i)*portDists)}
-                BusWidth = Some (busWidth i)
+                BusWidth = busWidth i
                 ConnectionDirection = connectionDirection
                 HostId = compProps.Id
             })
@@ -144,33 +166,43 @@ let generatePortList compProps numOfPorts portType connectionDirection =
 let generateBasicCompProps compType compId compPos =
     let ((numOfInputs:int), (numOfOutputs:int)) =
         match compType with 
-        | Not -> (1, 1)
+        | Not | BusSelection _ -> (1, 1)
         | And | Or | Xor | Nand | Nor | Xnor -> (2, 1)
         | Decode4 -> (2, 4)
+        | NbitsAdder _ -> (3, 2)
         | Input _ | Output _ | Constant _ -> (0, 1)
         | _ -> failwithf "should not occur"
 
+    /// A vertical extension at the top of the symbol that Left/Right Ports 
+    /// do not have access to.
     let headerMargin = 
         match compType with 
-        | Decode4 -> 20.
+        | Decode4 | NbitsAdder _ -> 20.
         | _ -> 0.
+
+    /// A vertical extension at the bottom of the symbol that Left/Right Ports 
+    /// do not have access to. 
     let footerMargin = 0.
 
     let height = 
         match compType with 
         | Not | And | Or | Xor | Nand | Nor | Xnor -> 50.
+        | Input _ | Output _ -> 25.
+        | Constant _ -> 20.
+        | BusSelection _ -> 30.
         | _ ->  headerMargin + footerMargin + 20. + float(20 * List.max [numOfInputs; numOfOutputs])
     let width = 
         match compType with 
         | Not | Nand | Nor | Xnor -> 60.
-        | Decode4 -> 80.
+        | Input _ | Output _ | Constant _ -> 35.
+        | Decode4 | NbitsAdder _ -> 80.
         | _ -> 50.  
     
     let compProps = {
         Id = compId
         Pos = compPos
         Type = compType
-        NumOfUpInputs = 0
+        NumOfUpwardInputs = 0
         H = height
         W = width
         HeaderMargin = headerMargin
@@ -197,16 +229,24 @@ let generateMuxProps compType compId compPos =
         | DemuxN n -> (1, (log2 n), n)
         | _ -> failwithf "should not occur"
 
+    /// A vertical extension at the top of the symbol that Left/Right Ports 
+    /// do not have access to.
     let headerMargin = 20. + float(numOfSelects*10)
+
+    /// A vertical extension at the bottom of the symbol that Left/Right Ports 
+    /// do not have access to. 
     let footerMargin = 10. + float(numOfSelects*10)
+
+    let height = headerMargin + footerMargin + 20. + float(20 * List.max [numOfDataInputs; numOfOutputs])
+    let width = 25. + float(25*numOfSelects)
 
     let compProps = {
             Id = compId
             Pos = compPos
             Type = compType
-            NumOfUpInputs = numOfSelects
-            H = headerMargin + footerMargin + 20. + float(20 * List.max [numOfDataInputs; numOfOutputs])
-            W = 25. + float(25*numOfSelects)
+            NumOfUpwardInputs = numOfSelects
+            H = height
+            W = width
             HeaderMargin = headerMargin
             FooterMargin = footerMargin
         }
@@ -222,10 +262,10 @@ let generateMuxProps compType compId compPos =
 /// in its initial form. This is called by the AddSymbol message and need not be exposed.
 let createNewSymbol (compType:CommonTypes.ComponentType) (label:string) (pos:XYPos) : CommonTypes.Component = 
     let compId = SymbolId (Helpers.uuid())
-    let (portList, height, width, numOfInputs, numOfOutputs, numOfUpInputs) = 
+    let (portList, height, width, numOfInputs, numOfOutputs, numOfUpwardInputs) = 
         match compType with
         | Not | And | Or | Xor | Nand | Nor | Xnor -> generateBasicCompProps compType compId pos 
-        | Decode4 -> generateBasicCompProps compType compId pos 
+        | Decode4 | BusSelection _ | NbitsAdder _ -> generateBasicCompProps compType compId pos 
         | Input bw | Output bw | Constant (bw,_) -> generateBasicCompProps compType compId pos
         | Mux2 | Demux2 | Mux4 | Demux4 | MuxN _ | DemuxN _ -> generateMuxProps compType compId pos
         | _ -> failwithf "Error: component not implemented"
@@ -236,7 +276,7 @@ let createNewSymbol (compType:CommonTypes.ComponentType) (label:string) (pos:XYP
         Ports = portList
         NumOfInputs = numOfInputs
         NumOfOutputs = numOfOutputs
-        NumOfUpInputs = numOfUpInputs
+        NumOfUpwardInputs = numOfUpwardInputs
         Pos = pos
         H = height
         W = width
@@ -246,12 +286,15 @@ let createNewSymbol (compType:CommonTypes.ComponentType) (label:string) (pos:XYP
 
 /// Dummy function for test. The real init would probably have no symbols.
 let init () =
-    List.allPairs [1..3] [1..1]
+    List.allPairs [1..2] [1..2]
     |> List.map (fun (x,y) -> {X = float (x*200+40); Y=float (y*200+40)})
     |> List.map (fun {X=x;Y=y} -> 
-        if y<x 
-        then (createNewSymbol (Xnor) "label" {X=x;Y=y}) 
-        else (createNewSymbol (Decode4) "label" {X=x;Y=y}))
+        if y=x 
+        then createNewSymbol (NbitsAdder 4) "label" {X=x;Y=y}
+        else 
+            if y<x
+            then (createNewSymbol (MuxN 16) "label" {X=x;Y=y})
+            else (createNewSymbol (And) "label" {X=x;Y=y}) )
     , Cmd.none
 
 /// update function which displays symbols
@@ -405,28 +448,41 @@ let private invertor (props:BasicSymbolProps) (color:string) (rectWidth:float) _
 
 let private portLabels (sym:Symbol) (i:int) =
     match sym.Type with
-    | Not | And | Or | Xor | Nand | Nor | Xnor | Input _ | Output _ -> text [] []
+    | Not | And | Or | Xor | Nand | Nor | Xnor 
+    | Input _ | Output _ | Constant _ | BusSelection _ -> text [] []
     | _ -> 
         let port = sym.Ports.[i]
+
         let (xMargin, yMargin, textAnchor, dominantBaseline) = 
             match port.ConnectionDirection with 
             | Right -> (5., 0., "start", "middle")
             | Left -> (-5., 0., "end", "middle")
             | Up -> (0., -6., "middle", "auto")
             | Down -> (0., 6., "middle", "hanging")
-        let labelPrefix, labelNum =
+
+        let portLabel =
             match sym.Type with 
+
             | Decode4 -> 
                 match port.PortType, port.PortNumber with
-                | PortType.Input, Some 0 -> "SEL", ""
-                | PortType.Input, _ -> "DATA", ""
-                | PortType.Output, _ -> "", string(i-sym.NumOfInputs)
+                | PortType.Input, Some 0 -> "SEL"
+                | PortType.Input, _ -> "DATA"
+                | PortType.Output, _ -> string(i-sym.NumOfInputs)
+
+            | NbitsAdder _ -> 
+                match port.PortType, port.PortNumber with
+                | PortType.Input, Some 0 -> "Cin"
+                | PortType.Input, Some 1 -> "A"
+                | PortType.Input, Some 2 -> "B"
+                | PortType.Output, Some 0 -> "Sum"
+                | PortType.Output, Some 1 -> "Cout"
+                | _ -> failwithf "should not occur"
+
             | _ ->
                 match port.PortType, port.ConnectionDirection with
-                | PortType.Input, Up -> "s", string(i-sym.NumOfInputs+sym.NumOfUpInputs)
-                | PortType.Input, _ -> "i", string(i)
-                | PortType.Output, _ -> "", string(i-sym.NumOfInputs)
-            //| _ -> failwithf "Error: should not occur"
+                | PortType.Input, Up -> "s" + string(i-sym.NumOfInputs+sym.NumOfUpwardInputs)
+                | PortType.Input, _ -> "i" + string(i)
+                | PortType.Output, _ -> "" + string(i-sym.NumOfInputs)
 
         text [ 
             X (port.RelativePortPos.X + xMargin); 
@@ -438,7 +494,7 @@ let private portLabels (sym:Symbol) (i:int) =
                 FontWeight "Bold"
                 Fill "Black" 
             ]
-        ] [str <| $"{labelPrefix}{labelNum}"] 
+        ] [str <| $"{portLabel}"] 
 
 
 let private renderBasicSymbol = 
@@ -453,6 +509,8 @@ let private renderBasicSymbol =
                     |> props.Dispatch
                 )
 
+            let numOfPorts = List.length(props.Sym.Ports)
+
             let fX = props.Sym.Pos.X
             let fY = props.Sym.Pos.Y
             let fW = 
@@ -460,7 +518,7 @@ let private renderBasicSymbol =
                 | Not | Nand | Nor | Xnor -> props.Sym.W - 10.
                 | _ -> props.Sym.W
             let fH = props.Sym.H
-            let n = List.length(props.Sym.Ports)
+
 
             let muxCut = 
                 match props.Sym.Type with 
@@ -476,11 +534,76 @@ let private renderBasicSymbol =
                 | DemuxN n -> 10. + float(log2 n)*10.
                 | _ -> 0.
 
+            let (p1:XYPos, p2:XYPos, p3:XYPos, p4:XYPos, p5:XYPos, p6:XYPos, p7:XYPos, p8:XYPos) =
+                match props.Sym.Type with 
+                
+                | Mux2 | Mux4 | MuxN _ -> 
+                    {X = 0.; Y = 0.}, {X = 0.; Y = fH}, {X = fW; Y = fH-muxCut}, {X = fW; Y = muxCut}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}
+                
+                | Demux2 | Demux4 | DemuxN _ -> 
+                    {X = 0.; Y = demuxCut}, {X = 0.; Y = fH-demuxCut}, {X = fW; Y = fH}, {X = fW; Y = 0.}, {X = 0.; Y = demuxCut}, {X = 0.; Y = demuxCut}, {X = 0.; Y = demuxCut}, {X = 0.; Y = demuxCut}
+                
+                | Input _ ->
+                    {X = 0.; Y = 0.}, {X = 0.; Y = fH}, {X = fW-10.; Y = fH}, {X = fW; Y = fH/2.}, {X = fW-10.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}
+                
+                | Output _ ->
+                    {X = 10.; Y = 0.}, {X = 0.; Y = fH/2.}, {X = 10.; Y = fH}, {X = fW; Y = fH}, {X = fW; Y = 0.}, {X = 10.; Y = 0.}, {X = 10.; Y = 0.}, {X = 10.; Y = 0.}
+
+                | Constant _ -> 
+                    {X = 0.; Y = 0.}, {X = 0.; Y = fH}, {X = 2.*fW/5.; Y = fH/2.}, {X = fW; Y = fH/2.}, {X = 2.*fW/5.; Y = fH/2.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}
+
+                | BusSelection _ -> 
+                    {X = 0.; Y = 0.}, {X = 0.; Y = fH}, {X = fW/2.; Y = fH}, {X = 3.*fW/4.; Y = 4.*fH/5.}, {X = fW; Y = 4.*fH/5.}, {X = fW; Y = fH/5.}, {X = 3.*fW/4.; Y = fH/5.}, {X = fW/2.; Y = 0.}
+  
+                | _ -> 
+                    {X = 0.; Y = 0.}, {X = 0.; Y = fH}, {X = fW; Y = fH}, {X = fW; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}, {X = 0.; Y = 0.}
+
+            
+            // let vertex5 : XYPos = {X = 0.; Y = fH/2.}
+            // let vertex6 : XYPos = {X = fW; Y = fH/2.}
+
+            // let cutRightW =
+            //     match props.Sym.Type with 
+            //     | Input _ -> 10.
+            //     | Constant _ -> 3.*fW/5.
+            //     | _ -> 0.
+
+            // let cutLeftW =
+            //     match props.Sym.Type with 
+            //     | Output _ -> 10.
+            //     | _ -> 0.
+
+            // let cutRightH = 
+            //     match props.Sym.Type with 
+            //     | Constant _ -> fH/2.
+            //     | Mux2 -> 20.
+            //     | Mux4 -> 30.
+            //     | MuxN n -> 10. + float(log2 n)*10.
+            //     | _ -> 0.
+
+            // let cutLeftH = 
+            //     match props.Sym.Type with 
+            //     | Demux2 -> 20.
+            //     | Demux4 -> 30.
+            //     | DemuxN n -> 10. + float(log2 n)*10.
+            //     | _ -> 0.
+
             let headerMargin = 
                 match props.Sym.Type with 
+                | Constant _ -> 20.
                 | Mux2 | Mux4 | MuxN _ -> muxCut + 5.
                 | Demux2 | Demux4 | DemuxN _ -> demuxCut + 5.
                 | _ -> 15.
+
+            let headerTextAnchor =
+                match props.Sym.Type with
+                | Constant _ -> "start"
+                | _ -> "middle"
+
+            let headerFontSize =
+                match props.Sym.Type with
+                | BusSelection _ -> "10px"
+                | _ -> "13px"
 
             let header = 
                 match props.Sym.Type with
@@ -489,6 +612,13 @@ let private renderBasicSymbol =
                 | Or | Nor -> ">=1"
                 | Xor | Xnor -> "=1"
                 | Decode4 -> "Decode"
+                | NbitsAdder bw -> "Adder (" + string(bw-1) + ":0)" 
+                | Input _ | Output _ -> ""
+                | Constant (_,v) -> System.Convert.ToString(v, 16)
+                | BusSelection (bw,lsb) -> 
+                    if bw = 1 
+                    then string(lsb)
+                    else "[" + string(bw+lsb-1) + ".." + string(lsb) + "]"
                 | Mux2 | Mux4 | MuxN _ -> "Mux"
                 | Demux2 | Demux4 | DemuxN _ -> "Demux"
                 | _ -> "symbol not recognized"
@@ -524,7 +654,8 @@ let private renderBasicSymbol =
                                 document.addEventListener("mousemove", handleMouseMove.current)
                             )
 
-                            SVGAttr.Points $"{0.},{demuxCut} {0.},{fH-demuxCut} {fW},{fH-muxCut} {fW},{muxCut}"
+                            SVGAttr.Points $"{p1.X},{p1.Y} {p2.X},{p2.Y} {p3.X},{p3.Y} {p4.X},{p4.Y} {p5.X},{p5.Y} {p6.X},{p6.Y} {p7.X},{p7.Y} {p8.X},{p8.Y}"
+                            //SVGAttr.Points $"{cutLeftW},{cutLeftH} {vertex5.X},{vertex5.Y} {cutLeftW},{fH-cutLeftH} {fW-cutRightW},{fH-cutRightH} {vertex6.X},{vertex6.Y} {fW-cutRightW},{cutRightH}"
                             SVGAttr.StrokeWidth "2px"
                             SVGAttr.Stroke "Black"
                             SVGAttr.FillOpacity 0.1
@@ -539,15 +670,15 @@ let private renderBasicSymbol =
                         X (fW/2.); 
                         Y headerMargin; 
                         Style [
-                            TextAnchor "middle" 
+                            TextAnchor headerTextAnchor
                             DominantBaseline "middle" 
-                            FontSize "13px"
+                            FontSize headerFontSize
                             FontWeight "Bold"
                             Fill "Black" 
                         ]
                     ] [str <| sprintf $"{header}"] 
 
-            ] @ (List.map (portLabels props.Sym) [0..n-1]) @ (List.map (invertor props color fW) [0]) ) 
+            ] @ (List.map (portLabels props.Sym) [0..numOfPorts-1]) @ (List.map (invertor props color fW) [0]) ) 
 
            
     , "BasicSymbol"
@@ -564,8 +695,7 @@ let renderSymbols (dispatch : Msg -> unit) (symToRender : Symbol) : ReactElement
                 Dispatch = dispatch
                 Key = string(symToRender.Id)
             })
-        | Not | And | Or | Xor | Nand | Nor | Xnor | Decode4 
-        | Mux2 | Demux2 | Mux4 | Demux4 | MuxN _ | DemuxN _ -> 
+        | _ -> 
             (renderBasicSymbol 
             {
                 Sym = symToRender
@@ -664,6 +794,8 @@ let getPortsFromId (symbolId : SymbolId) (symbolModel : Model) : Map<string,Port
     sym.Ports
     |> List.map (fun port -> (port.Id,port))
     |> Map.ofList 
+
+
 
 
 
